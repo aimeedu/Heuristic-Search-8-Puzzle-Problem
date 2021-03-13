dcf0b6ffd809d0ab217cf29ee69a6988c267d2c6
Aimee <aimeedu16@gmail.com>
2021-03-13 00:20:41 -0500

    working on manhattan heuristic A*

--- main.java
+++ main.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class main{
     public static void main(String args[]){
-        int d = 3; // dimension
+        int dim = 3; // dimension
         // use 0 to represent the empty tile. use 1D array
         List<Integer> goal = Arrays.asList(1,2,3,8,0,4,7,6,5);
         List<Integer> hard = Arrays.asList(2,8,1,4,6,3,0,7,5);
         
         // init a puzzle
-        puzzle p = new puzzle(d, goal, hard);
+        Puzzle p = new Puzzle(dim, goal, hard);
         
         // 1. misplaced heuristic A*
         p.solve_misplaced();
@@ -21,35 +21,116 @@ public class main{
     }
 }
 
-class puzzle{
+class Puzzle{
     // calculate Misplaced heuristics
     // h_misplaced(goal, hard);
     // calculate Manhattan heuristics
-    // h_manhattan(d, goal, hard);
-    int d, h_misplaced, h_manhattan;
-    
-    List<Integer> goal;
-    List<Integer> board;
-
-    List<Integer> open = new ArrayList<>();
-    List<Integer> closed = new ArrayList<>();
+    // h_manhattan(dim, goal, hard);
+    int dim, h_misplaced, h_manhattan;
+    Node goal;
+    Node start;
+    PriorityQueue<Node> open, close;
 
     // constructor
-    public puzzle(int d, List<Integer> goal, List<Integer> board){
-        this.d = d;
-        this.goal = goal;
-        this.board = board;
+    public Puzzle(int dim, List<Integer> goal_arr, List<Integer> start_arr){
+        this.dim = dim;
+        this.goal = new Node(this.dim, goal_arr, 0, 0, 0);
+        this.start = new Node(this.dim, 0, start_arr);
     }
 
     // in class methods
     public void solve_misplaced(){
 
     }
-
-    public void solve_manhattan(){
-
+    
+    public List<List<Integer>> getNeighbors(List<Integer> board){
+        List<List<Integer>> neighbors = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> done = new ArrayList<>();
+        List<Integer> left = new ArrayList<>();
+        List<Integer> up = new ArrayList<>();
+        int empty = board.indexOf(0);
+        for (int i = 0; i<board.size(); i++){
+            // right
+            if (empty % 3 != 2){
+                if(i == empty){
+                    right.add(board.get(i+1));
+                }else if(i == empty+1){
+                    right.add(0);
+                }else{
+                    right.add(board.get(i));
+                }  
+            }
+            // done
+            if (empty+3 < board.size()){
+                if(i == empty){
+                    done.add(board.get(i+3));
+                }else if(i == empty+3){
+                    done.add(0);
+                }else{
+                    done.add(board.get(i));
+                }
+            }
+            // left
+            if (empty % 3 != 0){
+                if(i == (empty-1)){
+                    left.add(0);
+                }else if(i == empty){
+                    left.add(board.get(empty-1));
+                }else{
+                    left.add(board.get(i));
+                }
+            }
+            // up
+            if (empty-3 >= 0){
+                if(i == (empty-3)){
+                    up.add(0);
+                }else if(i == empty){
+                    done.add(board.get(i-3));
+                }else{
+                    done.add(board.get(i));
+                }
+            }
+        }
+        neighbors.add(right);
+        neighbors.add(done);
+        neighbors.add(left);
+        neighbors.add(up);
+        return neighbors;
     }
 
+    public void solve_manhattan(){
+        boolean end = false;
+        // {board1, board2, ....} the board is represent by a list of 9 integers.
+        open = new PriorityQueue<>((a,b) -> a.f - b.f);
+        open.add(start);
+        close = new PriorityQueue<>();
+        int g = 0;
+        while(!end){
+            Node cur = open.poll(); // get the best node in the open list. least f(n).
+            if (cur.board == goal.board){
+                end = true;
+                return;
+            }else{
+                close.add(cur); // expand
+                // build all neighbor's board after move the white tile, and check one by one.
+                List<List<Integer>> neighbors = getNeighbors(cur.board);
+                for (List<Integer> n : neighbors){
+                    if (close.contains(n) && g<close.get(n).g){
+
+                    }else if(open.contains(n) && g<open.get(n).g){
+
+                    }else{
+                        Node next = new Node(dim,g,n); // set g.
+                        next.h_manhattan(dim, goal.board, n); // calculate h and f.
+                        open.add(next);
+                    }
+                }
+            }
+            g++;
+        }
+     
+    }
     public void solve_IDA_manhattan(){
 
     }
@@ -57,9 +138,36 @@ class puzzle{
     public void solve_DFBB_manhattan(){
 
     }
+}
+
+class Node{
+    int dim;
+    int g, h, f;
+    List<Integer> goal;
+    List<Integer> board;
+    Node parent;
+
+    // constructors
+    public Node(int dim, List<Integer> board,int g, int h, int f){ // for goal board.
+        this.dim = dim;
+        this.board = board; 
+        this.g = g;
+        this.h = h;
+        this.f = f;
+    }
+
+    public Node(int dim, int g, List<Integer> board){
+        this.dim = dim;
+        this.g = g;
+        this.board = board; 
+        // this.h = this.h_manhattan(goal, board, g);
+        // this.f = h+g;
+    }
+
+    // methods
 
     // calculate Misplaced heuristics
-    public int h_misplaced(List<Integer> goal, List<Integer> board){
+    public int h_misplaced(List<Integer> goal, List<Integer> board, int g){
         int h = 0;
         for(int i=0; i<goal.size(); i++){
             int tile = goal.get(i);
@@ -67,19 +175,24 @@ class puzzle{
                 h++;
             }
         }
+        this.h = h;
+        this.f = h + g;
         return h;
     } 
+    
     // calculate Manhattan heuristics
-    public int h_manhattan(int d, List<Integer> goal, List<Integer> board) {
+    public int h_manhattan(int dim, List<Integer> goal, List<Integer> board) {
         int h = 0;
         for(int i = 0; i<goal.size(); i++){
             int tile = goal.get(i);
             if(tile != 0){
                 int board_index = board.indexOf(tile);
-                int step = Math.abs(i%d - board_index%d) +  Math.abs(i/d - board_index/d);
+                int step = Math.abs(i%dim - board_index%dim) +  Math.abs(i/dim - board_index/dim);
                 h += (step); 
             }
         }
+        this.h = h;
+        this.f = h + g;
         return h;
     }
 }
